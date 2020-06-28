b78b7ccf991660da059e0e599b1fcc6f847e16b0
amritsrivastava <srivastava.amrit1999@gmail.com>
2020-06-28 09:36:38 +0530

    Integrate flux with UI layer

--- src/Application.js
+++ src/Application.js
@@ -5,36 +5,19 @@ import Input from './Input';
 import Result from './Result';
 
 import calculatePizzasNeeded from './lib/calculate-pizzas-needed';
+import PizzaCalculatorStore from './PizzaCalculatorStore';
+import * as actions from './actions';
 
-const initialState = {
-  numberOfPeople: 10,
-  slicesPerPerson: 2,
-};
-
-export default class Application extends Component {
-  state = { ...initialState };
-
-  updateNumberOfPeople = event => {
-    const numberOfPeople = parseInt(event.target.value, 10);
-    this.setState({ numberOfPeople });
-  };
-
-  updateSlicesPerPerson = event => {
-    const slicesPerPerson = parseInt(event.target.value, 10);
-    this.setState({ slicesPerPerson });
-  };
-
-  reset = event => {
-    this.setState({ ...initialState });
-  };
-
+class PizzaCalculator extends Component {
   render() {
-    const { numberOfPeople, slicesPerPerson } = this.state;
-    const numberOfPizzas = calculatePizzasNeeded(
+    const {
       numberOfPeople,
       slicesPerPerson,
-    );
-
+      numberOfPizzas,
+      updateNumberOfPeople,
+      updateSlicesPerPerson,
+      reset,
+    } = this.props;
     return (
       <div className="Application">
         <Title />
@@ -43,20 +26,64 @@ export default class Application extends Component {
           type="number"
           min={0}
           value={numberOfPeople}
-          onChange={this.updateNumberOfPeople}
+          onChange={updateNumberOfPeople}
         />
         <Input
           label="Slices Per Person"
           type="number"
           min={0}
           value={slicesPerPerson}
-          onChange={this.updateSlicesPerPerson}
+          onChange={updateSlicesPerPerson}
         />
         <Result amount={numberOfPizzas} />
-        <button className="full-width" onClick={this.reset}>
+        <button className="full-width" onClick={reset}>
           Reset
         </button>
       </div>
+    );
+  }
+}
+
+export default class Application extends Component {
+  state = PizzaCalculatorStore.getState();
+
+  updateNumberOfPeople = (event) => {
+    const numberOfPeople = parseInt(event.target.value, 10);
+    actions.updateNumberOfPeople(numberOfPeople);
+  };
+
+  updateSlicesPerPerson = (event) => {
+    const slicesPerPerson = parseInt(event.target.value, 10);
+    actions.updateSlicesPerPerson(slicesPerPerson);
+  };
+
+  updateState = () => {
+    this.setState(PizzaCalculatorStore.getState());
+  };
+
+  componentDidMount() {
+    PizzaCalculatorStore.on('change', this.updateState);
+  }
+
+  componentWillUnmount() {
+    PizzaCalculatorStore.off('change', this.updateState);
+  }
+
+  render() {
+    const { numberOfPeople, slicesPerPerson } = this.state;
+    const numberOfPizzas = calculatePizzasNeeded(
+      numberOfPeople,
+      slicesPerPerson
+    );
+
+    return (
+      <PizzaCalculator
+        {...this.state}
+        numberOfPizzas={numberOfPizzas}
+        updateNumberOfPeople={this.updateNumberOfPeople}
+        updateSlicesPerPerson={this.updateSlicesPerPerson}
+        reset={actions.reset}
+      />
     );
   }
 }
